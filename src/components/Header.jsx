f488b8c38a8e960256b013691a709088cd4a9d58
colin dodds <colindodds@colins-MacBook-Pro.local>
2023-05-18 12:59:45 +0100

    changed logo font and widened background image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import styles from "./Header.module.css";
-import WebFont from 'webfontloader';
 
 export default () => (
   <React.Fragment>
     <Navbar className={styles.logo}>
-      <Navbar.Brand href="#home" style={{color: '#b5232c'}}>Touch Type Tester</Navbar.Brand>
+      <Navbar.Brand href="#home" style={{color: '#b5232c', fontSize: '20pt'}}>Touch Type Tester</Navbar.Brand>
     </Navbar>
   </React.Fragment>
 );
